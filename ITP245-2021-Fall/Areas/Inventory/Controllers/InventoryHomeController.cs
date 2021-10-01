7cef24ca4a76e11d644bad689256691496ffc0f1
Michael Bourlotos <mbourlotos5001@email.vccs.edu>
2021-09-30 20:21:33 -0400

    Submit Home Page Assignment

---
+++ ITP245-2021-Fall/Areas/Inventory/Controllers/InventoryHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITP245_2021_Fall.Areas.Inventory.Controllers
+{
+    public class InventoryHomeController : Controller
+    {
+        // GET: Inventory/InventoryHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ITP245-2021-Fall/Areas/School/Controllers/SchoolHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITP245_2021_Fall.Areas.School.Controllers
+{
+    public class SchoolHomeController : Controller
+    {
+        // GET: School/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
