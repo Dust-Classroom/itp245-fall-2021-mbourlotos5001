0dadaafc19ee8bd1ed3bb1184db680f670d09f98
Michael Bourlotos <mbourlotos5001@email.vccs.edu>
2021-10-19 19:37:58 -0400

    Scaffolding Assignment

---
+++ ITP245-Model/Inventory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ITP245_Model
+{
+    [MetadataType(typeof(ItemMetaData))]
+  public partial class Item
+    {
+        private sealed class ItemMetaData
+        {
+         
+            [Display(Name = "Item Title")]
+            public string Name { get; set; }
+
+       
+
+
+
+        }
+    }
+    [MetadataType(typeof(CategoryMetaData))]
+    public partial class Category
+    {
+        private sealed class CategoryMetaData
+        {
+            [Display(Name = "Format")]
+            public string Name { get; set; }
+
+        
+
+
+
+        }
+    }
+    [MetadataType(typeof(VendorMetaData))]
+    public partial class Vendor
+    {
+        private sealed class VendorMetaData
+        {
+            [Display(Name = "Vendor")]
+            public string Name { get; set; }
+
+
+        }
+    }
+
+}
